9aa1f7ebb230721b5db21065b15200d8bce796ef
BoomCheeseBurge <brandonjohnlyc@gmail.com>
2024-10-27 21:16:47 +0700

    Tenth Video Result

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -41,4 +41,14 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+    /**
+     * Assign a model as an admin.
+     */
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_admin' => true,
+        ]);
+    }
 }
