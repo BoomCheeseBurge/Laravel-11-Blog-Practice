83d43fc04098901d7196ed7eb5239ac2d0734e64
BoomCheeseBurge <brandonjohnlyc@gmail.com>
2024-10-30 18:34:35 +0700

    Thirteenth Video Result

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create a single user without User factory
+        User::create([
+            'fullname' => 'John Doe',
+            'username' => 'doe123',
+            'email' => 'doe@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        // Generate random users using the User factory
+        User::factory(5)->create();
+    }
+}
